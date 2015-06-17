fca4e03132572ff99ac63063b0565f74f4430fc0
Lorenzo Cocchi <lorenzo.cocchi@softecspa.it>
2015-06-17 17:11:28 +0200

    add export xva

--- utils/snapback.sh
+++ utils/snapback.sh
@@ -33,12 +33,17 @@ SNAPSHOT_SUFFIX="snapback"
 TEMP_SUFFIX="newbackup"
 # Temporary file
 TEMP="/tmp/snapback.$$"
-# UUID of the destination SR for backups
-DEST_SR="3be6f5c2-8828-8ed4-2c9b-db78a96b3a99"
+
+# UUID of the destination SR for template backups
+TMPL_SR="3be6f5c2-8828-8ed4-2c9b-db78a96b3a99"
+# mount point of the destination SR for xva backups
+XVA_SR="/var/run/sr-mount/3be6f5c2-8828-8ed4-2c9b-db78a96b3a99"
+
 # slack
-SLACK_URL="https://xxx"
-SLACK_CHANNEL="#xxx"
-SLACK_USERNAME="xxx"
+SLACK_URL="https://hooks.slack.com/services/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"
+SLACK_CHANNEL="#ops"
+SLACK_USERNAME="webhookbot"
+
 # script name
 SELF=${0##*/}
 
@@ -108,8 +113,8 @@ delete_snapshot()
     done
 
     # Now we can remove the snapshot itself
-    log "removing snapshot with UUID: $DELETE_SNAPSHOT_UUID"
-    xe snapshot-uninstall uuid=$DELETE_SNAPSHOT_UUID force=true
+    log "removing snapshot with UUID: $SNAPSHOT_UUID"
+    xe snapshot-uninstall uuid=$SNAPSHOT_UUID force=true
 }
 
 # See above - templates also seem to leave stray VDIs around...
@@ -125,23 +130,66 @@ delete_template()
     done
 
     # Now we can remove the template itself
-    log "removing template with UUID: $DELETE_TEMPLATE_UUID"
-    xe template-uninstall template-uuid=$DELETE_TEMPLATE_UUID force=true
+    log "removing template with UUID: $TEMPLATE_UUID"
+    xe template-uninstall template-uuid=$TEMPLATE_UUID force=true
+}
+
+slack_msg()
+{
+    local MSG=$1
+
+    curl -X POST -d \
+        "payload={
+            \"channel\": \"$SLACK_CHANNEL\",
+            \"username\": \"$SLACK_USERNAME\",
+            \"text\": \"$SELF: $MSG\"
+        }" \
+        $SLACK_URL
+}
+
+check_sr_mount()
+{
+    local SR=$1
+    local FILE=${2}
+    local STR=${3:-'mounted'}
+
+    if [ -z "$FILE" ]; then
+        FILE=.${SELF/.sh/}
+    fi
+
+    SR_MOUNT=$(mount | grep $SR | awk '{print $3 }')
+
+    if [ -z "$SR_MOUNT" ]; then
+        return 1
+    fi
+
+    if grep -q $STR ${SR_MOUNT}/${FILE} 2>/dev/null; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 LOCKFILE=/tmp/snapback.lock
 
 if [ -f $LOCKFILE ]; then
     log "lockfile $LOCKFILE exists, exiting!"
-    curl -X POST -d \
-        "payload={
-            \"channel\": \"$SLACK_CHANNEL\",
-            \"username\": \"$SLACK_USERNAME\",
-            \"text\": \"$SELF: failed: lockfile $LOCKFILE exists, exiting!\"
-        }" \
-        $SLACK_URL
+    slack_msg "failed: lockfile $LOCKFILE exists, exiting!"
     exit 1
 fi
+
+if ! check_sr_mount $TMPL_SR; then
+    log "failed: SR: $TMPL_SR not mounted"
+    slack_msg "failed: SR: $TMPL_SR not mounted"
+    exit 1
+fi
+
+if ! check_sr_mount $XVA_SR; then
+    log "failed: SR: $XVA_SR not mounted"
+    slack_msg "failed: SR: $XVA_SR not mounted"
+    exit 1
+fi
+
 
 touch $LOCKFILE
 
@@ -200,10 +248,12 @@ for VM in $RUNNING_VMS; do
         name-label=$VM_NAME-$SNAPSHOT_SUFFIX | xe_param uuid)
 
     if [ "$VM_SNAPSHOT_CHECK" != "" ]; then
-        log "found old backup snapshot: $VM_SNAPSHOT_CHECK, Deleting..."
-        delete_snapshot $VM_SNAPSHOT_CHECK
+        for SNAPSHOT in $VM_SNAPSHOT_CHECK; do
+            log "found old backup snapshot with UUID: $SNAPSHOT, Deleting..."
+            delete_snapshot $SNAPSHOT
+        done
     fi
-
+    
     log "creating snapshot backup"
 
     # Select appropriate snapshot command
@@ -223,46 +273,64 @@ for VM in $RUNNING_VMS; do
 
     if [ $SNAPSHOT_UUID_RET -ne 0 ]; then
         log "failed: created snapshot"
-        curl -X POST -d \
-            "payload={
-                \"channel\": \"$SLACK_CHANNEL\",
-                \"username\": \"$SLACK_USERNAME\",
-                \"text\": \"$SELF: $VM_NAME: failed: created snapshot\"
-            }" \
-        $SLACK_URL
+        slack_msg "$VM_NAME: failed: created snapshot"
         continue
     fi
 
     log "created snapshot with UUID: $SNAPSHOT_UUID"
-    log "copying snapshot with UUID: $SNAPSHOT_UUID to SR: $DEST_SR"
+    log "copying snapshot with UUID: $SNAPSHOT_UUID to SR: $TMPL_SR"
     # Check there isn't a stale template with TEMP_SUFFIX name hanging
     # around from a failed job
     TEMPLATE_TEMP="$(xe template-list name-label="$VM_NAME-$TEMP_SUFFIX" | \
         xe_param uuid)"
 
     if [ "$TEMPLATE_TEMP" != "" ]; then
-        log "found a stale temporary template, removing UUID $TEMPLATE_TEMP"
-        delete_template $TEMPLATE_TEMP
+        for TEMPLATE in $TEMPLATE_TEMP; do
+            log "found a stale temporary template, removing UUID: $TEMPLATE"
+            delete_template $TEMPLATE
+        done
     fi
 
-    TEMPLATE_UUID=$(xe snapshot-copy uuid=$SNAPSHOT_UUID sr-uuid=$DEST_SR \
+    TEMPLATE_UUID=$(xe snapshot-copy uuid=$SNAPSHOT_UUID sr-uuid=$TMPL_SR \
         new-name-description="Snapshot created on $(date)" \
         new-name-label="$VM_NAME-$TEMP_SUFFIX")
 
     TEMPLATE_UUID_RET=$?
 
-    log "removing temporary snapshot backup"
+    if [ $TEMPLATE_UUID_RET -ne 0 ]; then
+        log "failed: copy snapshot with UUID: $SNAPSHOT_UUID to SR: $TMPL_SR"
+        slack_msg \
+            "failed: copy snapshot with UUID: $SNAPSHOT_UUID to SR: $TMPL_SR"
+
+        log "removing temporary snapshot backup with UUID: $SNAPSHOT_UUID"
+        slack_msg \
+            "removing temporary snapshot backup with UUID: $SNAPSHOT_UUID"
+        delete_snapshot $SNAPSHOT_UUID
+
+        continue
+    fi
+
+    unset BACKUP_DIR
+    BACKUP_DIR=${XVA_SR}/${VM_NAME}
+
+    if [ ! -d $BACKUP_DIR ]; then
+        mkdir -p $BACKUP_DIR
+        log "create $BACKUP_DIR"
+    fi
+
+    unset VM_FILENAME
+    VM_FILENAME=${BACKUP_DIR}/${VM_NAME}-${BACKUP_DATE}.xva
+
+    log "export $VM_NAME to $VM_FILENAME"
+    xe vm-export vm=$SNAPSHOT_UUID filename=$VM_FILENAME
+    VM_EXPORT_RET=$?
+
+    log "removing temporary snapshot backup with UUID: $SNAPSHOT_UUID"
     delete_snapshot $SNAPSHOT_UUID
 
-    if [ $TEMPLATE_UUID_RET -ne 0 ]; then
-        log "failed: copy snapshot with UUID: $SNAPSHOT_UUID to SR: $DEST_SR"
-        curl -X POST -d \
-            "payload={
-                \"channel\": \"$SLACK_CHANNEL\",
-                \"username\": \"$SLACK_USERNAME\",
-                \"text\": \"$SELF: $VM_NAME: failed: copy snapshot to SR\"
-            }" \
-        $SLACK_URL
+    if [ $VM_EXPORT_RET -ne 0 ]; then
+        log "failed: export $VM_NAME to $VM_FILENAME"
+        slack_msg "failed: export $VM_NAME to $VM_FILENAME"
         continue
     fi
 
@@ -299,8 +367,8 @@ for VM in $RUNNING_VMS; do
     log "backup for $VM_NAME successfully"
 done
 
-xe vdi-list sr-uuid=$DEST_SR > /var/run/sr-mount/$DEST_SR/mapping.txt
-xe vbd-list > /var/run/sr-mount/$DEST_SR/vbd-mapping.txt
+xe vdi-list sr-uuid=$TMPL_SR > /var/run/sr-mount/$TMPL_SR/mapping.txt
+xe vbd-list > /var/run/sr-mount/$TMPL_SR/vbd-mapping.txt
 
 log "snapshot backup finished"
 
